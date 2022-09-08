c52a31b045c0e2c8d0591943162289caae2f4b84
drewingtonC137 <amjj.edwards@gmail.com>
2022-09-08 12:05:37 +0200

    myInfoUpdate

--- script/myInfo.js
+++ script/myInfo.js
@@ -1,10 +1,10 @@
 window.onload = function (){
-    var id = 69;
+    var id = 77;
     fetch("http://localhost:8082/getUser/" + id)
         .then(a => a.json())
         .then(b => {
                 console.log(b);
-                document.getElementById("go3").innerHTML +=b.name  + b.cityName +  + "<hr>"
+                document.getElementById("go3").innerHTML +=b.name + "\n " + b.description + " " + b.cityName+ " " + b.streetName+ " " + b.houseNumber+ " " + b.houseNumberAddition+ " " + b.zipCode  + "<hr>"
             
         })
     }      
