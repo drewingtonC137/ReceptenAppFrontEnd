28ea79f8eaba1d61d1b0d8527cf7339b7e60f570
Nomis89 <95224816+Nomis89@users.noreply.github.com>
2022-09-13 13:16:42 +0200

    commit

--- script/addStock.js
+++ script/addStock.js
@@ -1,23 +1,24 @@
 addQuantityTypesToSelect();
 
-function addQuantityTypesToSelect(){
+function addQuantityTypesToSelect() {
     fetch(url + "/returnQuantityTypes")
-        .then( response => response.json())
-        .then( 
-            data => { 
-                fillQuantityTypeSelect(data)})
+        .then(response => response.json())
+        .then(
+            data => {
+                fillQuantityTypeSelect(data)
+            })
 }
 
 function cleanString(string) {
-    string =  string.charAt(0).toUpperCase() + string.slice(1);
+    string = string.charAt(0).toUpperCase() + string.slice(1);
     return string.replace("_", " ");
-  }
+}
 
-function fillQuantityTypeSelect (data){
+function fillQuantityTypeSelect(data) {
     console.log("Fill select with data");
     var quantityTypeSelect = document.getElementById("quantityTypeSelect");
     var option;
-    for(var x = 0; x < data.length ; x++){
+    for (var x = 0; x < data.length; x++) {
         option = document.createElement("option");
         option.text = cleanString(data[x].toLowerCase());
         option.value = data[x];
@@ -30,39 +31,39 @@ function fillQuantityTypeSelect (data){
 const submitStock = document.getElementById("submitStock")
 submitStock.addEventListener("click", (e) => {
     console.log("adding stock item");
-   let StockObj = {};
+    let StockObj = {};
 
-   const ingredientName = document.getElementById("ingredientName").value;
-   const quantity = document.getElementById("quantity").value;
-   const expirationdate = document.getElementById("expirationDate").value;
-   const quantityType = document.getElementById("quantityTypeSelect").value;
+    const ingredientName = document.getElementById("ingredientName").value;
+    const quantity = document.getElementById("quantity").value;
+    const expirationdate = document.getElementById("expirationDate").value;
+    const quantityType = document.getElementById("quantityTypeSelect").value;
 
-   if (document.getElementById("availableToOthers").value == "on"){
+    if (document.getElementById("availableToOthers").value == "on") {
         availableToOthers = "true";
-   }
-   else {
+    }
+    else {
         availableToOthers = "false";
-   }
-//    const availableToOthers = document.getElementById("availableToOthers").value;
+    }
+    //    const availableToOthers = document.getElementById("availableToOthers").value;
 
-   console.log(document.getElementById("availableToOthers").value);
+    console.log(document.getElementById("availableToOthers").value);
 
-   StockObj.amount = quantity;
-   StockObj.amountType = quantityType;
-   StockObj.expirationDate = expirationdate;
-   StockObj.availableToOthers = availableToOthers;
+    StockObj.amount = quantity;
+    StockObj.amountType = quantityType;
+    StockObj.expirationDate = expirationdate;
+    StockObj.availableToOthers = availableToOthers;
 
-   fetch(url + "/addStock/" + localStorage.getItem("accountId") + "/" + ingredientName, {
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/json;charset=utf8'
-      },
-      body: JSON.stringify(StockObj)
-   })
-      .then(res => res.text())
-      .then(d => {
-         alert("Stock gemaakt")
-        //  window.location.assign("inventory.html")
-      })
+    fetch(url + "/addStock/" + localStorage.getItem("accountId") + "/" + ingredientName, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf8'
+        },
+        body: JSON.stringify(StockObj)
+    })
+        .then(res => res.text())
+        .then(d => {
+            alert("Stock gemaakt")
+            //  window.location.assign("inventory.html")
+        })
 })
 
